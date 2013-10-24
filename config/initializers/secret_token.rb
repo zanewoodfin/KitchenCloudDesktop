d3e53a2c06d62f32b7d7f46966e3e6b6166853eb
Zane <pzwoodfin@gmail.com>
2013-10-24 15:31:09 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KitchenCloudDesktop::Application.config.secret_key_base = 'd704fad35b0dd65038236be50192bcd4850a5d15f144ed8d8581f468fbc370aa7bbed7daee0dfa9c6bc33aa1c26a99aac77efc30b2e0a0a498b25771c7c284b8'
